35d410c1de1ca4dea3f6f8a4fe34dafdac3816eb
pabitramohandot <work.pabitramohan@gmail.com>
2026-07-04 13:42:43 +0530

    Exclude superadmin role from reminder popup notifications

--- src/components/NotificationBell.js
+++ src/components/NotificationBell.js
@@ -105,7 +105,7 @@ export default function NotificationBell({ userRole }) {
   };
 
   useEffect(() => {
-    if (userRole) {
+    if (userRole && userRole !== 'superadmin') {
       fetchNotifications();
       const interval = setInterval(fetchNotifications, 30000);
       return () => clearInterval(interval);
